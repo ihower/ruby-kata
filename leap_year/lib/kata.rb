bca979dc72f93b595f3a90b7a7f3e069977b7b6a
Wen-Tien Chang <ihower@gmail.com>
2014-10-09 20:14:47 +0800

    Refactor

--- leap_year/lib/kata.rb
+++ leap_year/lib/kata.rb
@@ -1,13 +1,3 @@
 def is_leap_year?(y)
-  if y % 4 == 0
-    if y % 100 == 0
-      if y % 400 == 0
-        true
-      else
-        false
-      end
-    else
-      true
-    end
-  end
+  y % 400 == 0 || ( y % 4 == 0 && y % 100 !=0 )
 end
