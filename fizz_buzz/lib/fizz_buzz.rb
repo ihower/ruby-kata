e1043c7fe2be77d81ca8c9faeb6307368ef8af11
Wen-Tien Chang <ihower@gmail.com>
2014-10-09 19:56:22 +0800

    Finish fizz_buzz

--- fizz_buzz/lib/fizz_buzz.rb
+++ fizz_buzz/lib/fizz_buzz.rb
@@ -1,7 +1,15 @@
 class FizzBuzz
 
   def convert(n)
-    n
+    if n % 15 == 0
+      "FizzBuzz"
+    elsif n % 3 == 0
+      "Fizz"
+    elsif n % 5 == 0
+      "Buzz"
+    else
+      n
+    end
   end
 
 end
--- fizz_buzz/spec/lib/fizz_buzz_spec.rb
+++ fizz_buzz/spec/lib/fizz_buzz_spec.rb
@@ -2,10 +2,22 @@ require_relative "../../lib/fizz_buzz"
 
 describe FizzBuzz do
 
-  it "should convert 1 to 1" do
-    converter = FizzBuzz.new
+  let(:converter){ converter = FizzBuzz.new }
 
+  it "should convert 1 to 1" do
     expect( converter.convert(1) ).to eq 1
+  end
+
+  it "should convert 3 to fizz" do
+    expect( converter.convert(3) ).to eq "Fizz"
+  end
+
+  it "should convert 5 to buzz" do
+    expect( converter.convert(5) ).to eq "Buzz"
+  end
+
+  it "should convert 15 to FizzBuzz" do
+    expect( converter.convert(15) ).to eq "FizzBuzz"
   end
 
 end
